f2d7cb0a7dac2c37de9c2aaac0c5bf917d2f95b6
dmjr9z84d6-sudo <dmjr9z84d6@privaterelay.appleid.com>
2026-07-02 06:56:12 +0200

    Delete mount.jsx

--- mount.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import PitchApp from "./PitchApp.jsx";
-
-const el = document.getElementById("root");
-if (el) {
-  createRoot(el).render(React.createElement(PitchApp));
-}
